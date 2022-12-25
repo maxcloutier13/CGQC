99eb05e464909427b81183fa89028481b5dc7d2a
Maxime Cloutier <mcloutier13@gmail.com>
2022-12-25 16:58:29 -0500

    Fix: moved custom guns to 2022

--- cfg_patches.hpp
+++ cfg_patches.hpp
@@ -50,18 +50,15 @@ class CfgPatches
 				   "Headgear_cgqc_helmet_heli_green",
 				   "Headgear_cgqc_helmet_heli_wood",
 				   "Headgear_cgqc_helmet_heli_wood",
-				   "cgqc_gun_p99_wood",
-				   "cgqc_gun_hk417_tremblay",
+				   "cgqc_gun_p99_wood"};
+				   /*"cgqc_gun_hk417_tremblay",
 				   "Weapon_cgqc_gun_spar17_canpat",
-				   "Weapon_cgqc_gun_spar17_wood"};
+				   "Weapon_cgqc_gun_spar17_wood"*/
 		// List of weapons (CfgWeapons classes) contained in the addon.
 		weapons[] = {"cgqc_beret_vanilla",
 					 "cgqc_beret_green",
 					 "cgqc_beret_blue",
 					 "cgqc_cap_green",
-					 "cgqc_gun_hk417_tremblay",
-					 "cgqc_gun_spar17_canpat",
-					 "cgqc_gun_spar17_wood",
 					 "cgqc_gun_p99_wood"};
 	};
 };
