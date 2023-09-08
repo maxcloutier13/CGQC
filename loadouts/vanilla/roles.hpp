1282c69c0c1bdb19faa81abfb251342a07f9349f
Maxime Cloutier <mcloutier13@gmail.com>
2023-09-08 00:33:53 -0400

    Fix: role stuff

--- loadouts/vanilla/roles.hpp
+++ loadouts/vanilla/roles.hpp
@@ -2,6 +2,7 @@ case "vanilla_rifleman":{
 	[player] call CGQC_fnc_loadoutPrep;
 	[] call CGQC_loadout_vanillaBasic;
 	[] call CGQC_loadout_vanillaRifleman;
+	["tl"] call CGQC_fnc_getRadioPresets;
 	["inf"] spawn cgqc_fnc_switchPerks;
 	cgqc_player_role = "Vanilla Rifleman";
 
@@ -10,6 +11,7 @@ case "vanilla_tl":{
 	[player] call CGQC_fnc_loadoutPrep;
 	[] call CGQC_loadout_vanillaBasic;
 	[] call CGQC_loadout_vanillaTL;
+	["tl"] call CGQC_fnc_getRadioPresets;
 	["tl"] spawn cgqc_fnc_switchPerks;
 	cgqc_player_role = "Vanilla TeamLeader";
 };
