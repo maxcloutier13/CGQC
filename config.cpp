94b7ac58465ff8595adf343ca49b514afe6b5e73
Maxime Cloutier <mcloutier13@gmail.com>
2023-09-15 11:43:47 -0400

    Ajout: Mainmenu CGQC spotlight

--- config.cpp
+++ config.cpp
@@ -136,3 +136,17 @@ class CfgSounds
 
 // Interface / UI modifications
 #include "cfg_interface.hpp"
+
+class CfgMainMenuSpotlight // RscDisplayMain >> Spotlight works but is considered obsolete since SPOTREP #00064
+{
+	class CGQC
+	{
+		text = ""; // Text displayed on the square button, converted to upper-case
+		textIsQuote = 0; // 1 to add quotation marks around the text
+		picture = "\CGQC\textures\CGQC_512.paa"; // Square picture, ideally 512x512
+		//video = "\a3\Ui_f\Video\spotlight_1_Apex.ogv"; // Video played on mouse hover
+		action = "connectToServer ['arma3.cgqc.ca', 2302, '']";
+		actionText = "Join CGQC server"; // Text displayed in top left corner of on-hover white frame
+		condition = "true"; // Condition for showing the spotlight
+	};
+};
