2a4c657480b661aa1c7134eab52afc6bf5762b03
Maxime Cloutier <mcloutier13@gmail.com>
2023-10-18 00:57:05 -0400

    Ajout: Custom fireteam colors

---
+++ cfg_markerColors.hpp
@@ -0,0 +1,116 @@
+class CfgMarkerColors
+{
+	class Default
+	{
+		scope = 1;
+	};
+	class ColorBlack: Default
+	{
+		name = "$STR_CFG_MARKERCOL_BLACK";
+		color[] = {0,0,0,1};
+		scope = 2;
+	};
+	class ColorRed: Default
+	{
+		name = "Spartan-1";
+		color[] = {0.9,0,0,1};
+		scope = 2;
+	};
+	class ColorYellow: Default
+	{
+		name = "Spartan-2";
+		color[] = {0,0,1,1};
+		scope = 2;
+
+	};
+	class ColorGreen: Default
+	{
+		name = "Spartan-3";
+		color[] = {0,0.8,0,1};
+		scope = 2;
+
+	};
+	class ColorBlue: Default
+	{
+		name = "Yellow";
+		color[] = {0.85,0.85,0,1};
+		Scope = 2;
+	};
+
+
+
+	/*
+
+	class ColorAzure: Default
+	{
+		name = "Support";
+		color[] = {0,0.996,0.996,1};
+		Scope = 2;
+	};
+	class ColorPink: Default
+	{
+		name = "Recon";
+		color[] = {0.965,0.522,0.69,1};
+		Scope = 2;
+	};
+	class ColorOrange1: Default
+	{
+		name = "Romeo";
+		color[] = {1,0.6,0,1};
+		Scope = 2;
+	};
+	class ColorBrown1: Default
+	{
+		name = "Zulu";
+		color[] = {0.5,0.25,0,1};
+		Scope = 2;
+	};
+	class ColorBrown: Default
+	{
+		name = "BLUFOR";
+		color[] = {0,0.3,0.6,1};
+		scope = 2;
+	};
+
+	class ColorWhite: Default
+	{
+		scope = 1;
+	};
+	class ColorWEST: Default
+	{
+		scope = 1;
+	};
+	class ColorEAST: Default
+	{
+		scope = 1;
+	};
+	class ColorGUER: Default
+	{
+		scope = 1;
+	};
+	class ColorCIV: Default
+	{
+		scope = 1;
+	};
+	class ColorUNKNOWN: Default
+	{
+		scope = 1;
+	};
+	class colorBLUFOR: ColorWEST
+	{
+		scope = 0;
+	};
+	class colorOPFOR: ColorEAST
+	{
+		scope = 0;
+	};
+	class colorIndependent: ColorGUER
+	{
+		scope = 0;
+	};
+	class colorCivilian: ColorCIV
+	{
+		scope = 0;
+	};
+	*/
+};
