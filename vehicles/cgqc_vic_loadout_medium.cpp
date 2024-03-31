94029b1834656469bf7d572669e4364945f4591b
Maxime Cloutier <mcloutier13@gmail.com>
2024-03-31 18:24:32 -0400

    Ajout: UGV drones

---
+++ vehicles/cgqc_vic_loadout_medium.cpp
@@ -0,0 +1,61 @@
+class TransportItems{
+   // Ammo -----------------------------------
+    class _xx_cgqc_bandolier_ammo
+    {
+        count = 6;
+        name = "cgqc_bandolier_ammo";
+    };
+    // Medic Kit -----------------------------------
+    class _xx_CGQC_ITEMS_MEDKIT
+    {
+        count = 1;
+        name = "cgqc_items_medkit";
+    };
+    // Individual Kit  ----------------------------------
+    class _xx_CGQC_ITEMS_IFAK
+    {
+        count = 6;
+        name = "cgqc_items_ifak";
+    };
+	class _xx_ACE_bodyBag
+	{
+		count = 5;
+		name = "ACE_bodyBag";
+	};
+    class _xx_ACE_M26_Clacker
+	{
+		count = 1;
+		name = "ACE_M26_Clacker";
+	};
+
+};
+class TransportWeapons{
+    class _xx_launch_NLAW_F
+    {
+        weapon = "launch_NLAW_F";
+        count = 1;
+    };
+    class _xx_rhs_weap_m72a7
+    {
+        weapon = "rhs_weap_m72a7";
+        count = 2;
+    };
+};
+
+class TransportBackpacks{};
+
+class TransportMagazines{
+	class _xx_rhsusf_m112_mag
+	{
+		count = 4;
+		magazine = "rhsusf_m112_mag";
+	};
+    class _xx_SLAMDirectionalMine_Wire_Mag
+	{
+		count = 2;
+		magazine = "SLAMDirectionalMine_Wire_Mag";
+	};
+};
+
+
+
